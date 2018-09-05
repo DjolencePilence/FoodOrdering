2857ce018b8e44ca13e73986052a287d501f0992
Djolence <djoleiblejana@gmail.com>
2018-09-05 18:36:54 +0200

    Restaurant info tab finished

---
+++ app/src/main/java/com/example/djole/foodordering/RestaurantImpressionsFragment.java
@@ -0,0 +1,32 @@
+package com.example.djole.foodordering;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+
+/**
+ * Created by Djole on 04-Sep-18.
+ */
+
+public class RestaurantImpressionsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.restaurant_impressions_tab, container, false);
+        return view;
+    }
+
+
+
+}
